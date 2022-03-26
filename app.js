7a1792b4a1a9c255d8a6022b24fd61ea96b79db7
Bhanu pratap Behera <bhanupratapbehera193@gmail.com>
2022-03-26 23:46:25 +0530

    github finder vnjs

---
+++ app.js
@@ -0,0 +1,29 @@
+// init github
+const github = new Github();
+// initui
+const ui = new UI();
+
+const searchUser = document.getElementById("search-user");
+// add event listener
+searchUser.addEventListener("keyup", (e) => {
+  // Get input text
+  const userText = e.target.value;
+
+  if (userText !== "") {
+    // Make http call
+    github.getUser(userText).then((data) => {
+      console.log(data);
+      if (data.profile.message === "Not Found") {
+        // Show alert
+        ui.showAlert("User Not Found", " alert alert-danger");
+      } else {
+        // Show profile
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    });
+  } else {
+    // Clear profile
+    ui.clearProfile();
+  }
+});
